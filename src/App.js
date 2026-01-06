7b8e60d9a220ccdc8a795afeeef9a5c85062d82b
teaonrocks <arxherchua@gmail.com>
2026-01-06 10:06:28 +0800

    lesson 16

--- src/App.js
+++ src/App.js
@@ -1,35 +1,61 @@
 import { useEffect, useState } from "react";
-const KEY = "";
+const KEY = "ebde49e4";
 
 function App() {
-  const [movies, setMovies] = useState([]);
+	const [movies, setMovies] = useState([]);
+	const [query, setQuery] = useState("");
 
-  fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=batman`)
-    .then((res) => res.json())
-    .then((data) => setMovies(data.Search));
+	useEffect(() => {
+		const controller = new AbortController();
+		async function fetchMovies(controller) {
+			try {
+				const response = await fetch(
+					`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+					{
+						signal: controller.signal,
+					}
+				);
+				const data = await response.json();
+				if (data.Response === "True") {
+					setMovies(data.Search);
+				} else {
+					setMovies([]);
+				}
+			} catch (error) {
+				console.error("Error fetching movies:", error);
+			}
+		}
+		fetchMovies(controller);
+		return () => controller.abort();
+	}, [query]);
 
-  return (
-    <div>
-      <h1>Movies</h1>
-      <input type="text" placeholder="Search movies..." />
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          {movies.map((movie) => (
-            <tr key={movie.imdbID}>
-              <td>{movie.Title}</td>
-              <td>{movie.Year}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Movies</h1>
+			<input
+				type="text"
+				placeholder="Search movies..."
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+			/>
+			<table>
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Year</th>
+					</tr>
+				</thead>
+				<tbody>
+					{movies.map((movie) => (
+						<tr key={movie.imdbID}>
+							<td>{movie.Title}</td>
+							<td>{movie.Year}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
